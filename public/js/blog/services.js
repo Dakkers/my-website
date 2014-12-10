27cebf7e61e2a2749540023ae8a2d85b4328de6a
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2014-12-09 23:53:25 -0500

    fix deadlinks; redirect blog to medium

--- public/js/blog/services.js
+++ public/js/blog/services.js
@@ -1,17 +1,17 @@
 var blogServices = angular.module('blogServices', ['ngResource']);
 
 blogServices.factory('Posts', ['$resource',
-	function ($resource) {
-		return $resource('/blog/api/:field/:data1/:data2', {
-			field: '@field',
-			data1: '@data1',
-			data2: '@data2'
-		}, {query: { method: 'GET', isArray: true } });
-	}
+    function ($resource) {
+        return $resource('/blog/api/:field/:data1/:data2', {
+            field: '@field',
+            data1: '@data1',
+            data2: '@data2'
+        }, {query: { method: 'GET', isArray: true } });
+    }
 ]);
 
 blogServices.factory('Sidebar', [ '$resource',
-	function ($resource) {
-		return $resource('/blog/api/sidebar');
-	}
+    function ($resource) {
+        return $resource('/blog/api/sidebar');
+    }
 ]);
