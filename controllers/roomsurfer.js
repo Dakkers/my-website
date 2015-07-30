ae73bdf4d945aadbb3e2820d63117abbaa1b95ec
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-07-30 19:55:39 -0400

    update roomsurfer to use postgres

--- controllers/roomsurfer.js
+++ controllers/roomsurfer.js
@@ -1,42 +1,69 @@
-var Room = require('./../app/models/room'),
-    Time = require('./../app/models/time'),
-    Building = require('./../app/models/building'),
+var path = require('path'),
+    pg         = require('pg'),
+    secrets    = require('./../secrets');
 
-    path = require('path');
-
+var conString = secrets.conString;
+var client = new pg.Client(conString);
+client.connect();
 
 exports.home = function(req, res) {
     res.sendFile(path.join(__dirname, '../public', 'roomsurfer.html'));
 };
 
 exports.usedrooms = function(req, res) {
-    Building.find({})
-    .select({_id: 0}).sort({building: 1}).lean()
-    .exec(function(err, data) {
-        res.json(data);
-    });
+    client.query(
+        "SELECT DISTINCT building, room FROM FreeRooms ORDER BY building, room",
+        function(err, result) { 
+            res.json(result.rows);
+        }
+    );
 };
 
-exports.building = function(req, res) {
-    Building.findOne({building: req.params.building.toUpperCase()})
-    .select({_id: 0}).lean()
-    .exec(function(err, data) {
-        res.json(data);
-    });
+exports.usedroomsBuilding = function(req, res) {
+    var building = req.params.building.toUpperCase();
+    client.query(
+        "SELECT DISTINCT room FROM FreeRooms WHERE building = ($1) ORDER BY room",
+        [building], function(err, result) {
+            res.json(result.rows);
+        }
+    );
 };
 
-exports.room = function(req, res) {
-    Room.findOne({room: req.params.room.replace("-"," ").toUpperCase()})
-    .select({_id: 0}).lean()
-    .exec(function(err, data) {
-        res.json(data);
-    });
+exports.roomBuilding = function(req, res) {
+    var building = req.params.building.toUpperCase();
+    client.query(
+        "SELECT room, day, starttime, endtime FROM FreeRooms WHERE building = ($1) ORDER BY room",
+        [building], function(err, result) {
+            res.json(result.rows);
+        }
+    );
 };
+
+exports.roomNumber = function(req, res) {
+    var building = req.params.building.toUpperCase(),
+        room     = req.params.room;
+    client.query(
+        "SELECT day, starttime, endtime FROM FreeRooms WHERE building = ($1) and room = ($2)",
+        [building, room], function(err, result) {
+            res.json(result.rows);
+        }
+    );
+}
 
 exports.time = function(req, res) {
-    Time.findOne({time: req.params.time.replace(/-/g,",")})
-    .select({_id: 0}).lean()
-    .exec(function(err, data) {
-        res.json(data);
-    });
+    var day   = req.params.day.toUpperCase(),
+    start = parseInt(req.params.start),
+    end   = parseInt(req.params.end);
+
+    if (!start)
+        start = 0;
+    if (!end)
+        end = 1439;
+
+    client.query(
+        "SELECT building, room FROM FreeRooms WHERE day = ($1) and starttime <= ($2) and endtime <= ($3)",
+        [day, start, end], function(err, result) {
+            res.json(result.rows);
+        }
+    )
 };
--- server.js
+++ server.js
@@ -2,14 +2,7 @@ var express = require('express'),
     app = express(),
     path = require('path'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
     projectData = require('./projects.json');
-
-var Post = require('./app/models/post'),
-    Room = require('./app/models/room'),
-    Time = require('./app/models/time'),
-    Building = require('./app/models/building');
-mongoose.connect('mongodb://localhost/stdako');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,22 +20,6 @@ app.get('/projects', function(req, res) {
 });
 
 // =====================================
-// BLOG STUFF ==========================
-// =====================================
-var blogRouter = express.Router({mergeParams: true}),
-    blogController = require('./controllers/blog');
-// app.use('/blog', blogRouter);
-
-blogRouter.route('/').get(blogController.home);
-blogRouter.route('/api').get(blogController.api);
-blogRouter.route('/api/posts/:range').get(blogController.range);
-blogRouter.route('/api/topic/:tag').get(blogController.topic);
-blogRouter.route('/api/archive/:year/:month?').get(blogController.archive);
-blogRouter.route('/api/sidebar').get(blogController.sidebar);
-blogRouter.route('/api/*').get(blogController.wildcardapi);
-blogRouter.route('/*').get(blogController.wildcard);
-
-// =====================================
 // ROOMSURFER ==========================
 // =====================================
 var roomsurferRouter = express.Router({mergeParams: true}),
@@ -51,9 +28,10 @@ app.use('/roomsurfer', roomsurferRouter);
 
 roomsurferRouter.route('/').get(roomsurferController.home);
 roomsurferRouter.route("/api/usedrooms").get(roomsurferController.usedrooms);
-roomsurferRouter.route("/api/usedrooms/:building").get(roomsurferController.building);
-roomsurferRouter.route('/api/room/:room').get(roomsurferController.room);
-roomsurferRouter.route('/api/time/:time').get(roomsurferController.time);
+roomsurferRouter.route("/api/usedrooms/:building").get(roomsurferController.usedroomsBuilding);
+roomsurferRouter.route('/api/room/:building').get(roomsurferController.roomBuilding);
+roomsurferRouter.route('/api/room/:building/:room').get(roomsurferController.roomNumber);
+roomsurferRouter.route('/api/time/:day/:start?/:end?').get(roomsurferController.time);
 
 // =====================================
 // STATIC SHIT =========================
